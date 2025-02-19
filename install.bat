9da097226e37ff4a41797d0d7a0063f791f32627
Diego Molina <77606137+SirDaXll@users.noreply.github.com>
2025-02-19 12:51:06 -0300

    Fix: Instalación de torch

--- install.bat
+++ install.bat
@@ -14,6 +14,9 @@ uv pip install -r requirements.txt
 echo Instalando dependencias de Flet...
 uv pip install flet[all]==0.26.0 --upgrade
 
+echo Instalando torch...
+uv pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu126
+
 echo Instalando pywhispercpp...
 uv pip install git+https://github.com/absadiki/pywhispercpp
 
--- storage/data/installwogit.bat
+++ storage/data/installwogit.bat
@@ -11,5 +11,8 @@ uv pip install -r requirements.txt
 echo Instalando dependencias de Flet...
 uv pip install flet[all]==0.26.0 --upgrade
 
+echo Instalando torch...
+uv pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu126
+
 echo ¡Instalación completada!
 pause
